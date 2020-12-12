a729027252e0b3e39893f9d57791f196e86f35fb
Edison Neto <ednetoali@gmail.com>
2020-12-12 14:54:42 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "root:password@tcp(localhost:5432)/todo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM todo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	fmt.Println(rows)
+}

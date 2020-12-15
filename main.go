9b333c5ad81b0d42b21c1f601384a19d307a4939
Edison Neto <ednetoali@gmail.com>
2020-12-15 00:26:07 -0300

    Change project to use database/sql instead of pgx functions

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 	"log"
 
-	"github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
 const DATABASE_URL = "postgres://user:123@localhost:5432/todo"
 
 func main() {
-	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	db, err := sql.Open("pgx", DATABASE_URL)
 	if err != nil {
 		log.Fatal("Connection failed\n", err)
 	}
-	defer conn.Close(context.Background())
+	defer db.Close()
 	var greeting string
-	err = conn.QueryRow(context.Background(), "select 'Hello, World!'").Scan(&greeting)
+	err = db.QueryRow("select 'Hello, World!'").Scan(&greeting)
 	if err != nil {
 		log.Fatal("QueryRow failed\n", err)
 	}

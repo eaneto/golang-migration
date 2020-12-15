bf43fa40c7deb8c4689254d2b548b682ccdfc636
Edison Neto <ednetoali@gmail.com>
2020-12-14 22:33:31 -0300

    Change module name

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/eaneto/todo-sql
+module github.com/eaneto/golang-sql
 
 go 1.14
 
--- main.go
+++ main.go
@@ -8,12 +8,13 @@ import (
 	"github.com/jackc/pgx/v4"
 )
 
+const DATABASE_URL = "postgres://user:123@localhost:5432/todo"
+
 func main() {
-	conn, err := pgx.Connect(context.Background(), "postgres://user:123@localhost:5432/todo")
+	conn, err := pgx.Connect(context.Background(), DATABASE_URL)
 	if err != nil {
 		log.Fatal("Connection failed\n", err)
 	}
-
 	defer conn.Close(context.Background())
 	var greeting string
 	err = conn.QueryRow(context.Background(), "select 'Hello, World!'").Scan(&greeting)

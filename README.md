d62914e03439c4f023f3ed0f9b85e35c799e3bc6
Edison Neto <edison.aguiar.neto@gmail.com>
2020-12-26 21:43:15 -0300

    Refactor to execute all scripts in a single transaction
    The migration as a whole should be atomic, so now all scripts execute
    inside a single transaction.

--- README.md
+++ README.md
@@ -6,5 +6,13 @@ Very basic tool to manage database migrations for PostgreSQL written in go.
 
 ```bash
 make
-./bin/golang-migration <database_name>
+./bin/golang-migration <user> <password> <database_name>
+```
+
+### With docker compose example
+
+```bash
+docker-compose up -d
+make
+./bin/golang-migration user 123 test
 ```
--- main.go
+++ main.go
@@ -13,104 +13,140 @@ import (
 )
 
 // TODO get user, password and host from CLI
-const DATABASE_URL = "postgres://user:123@localhost:5432/"
+const DATABASE_URL = "postgres://%s:%s@localhost:5432/%s"
 
-// TODO Create a table to registered processed scripts.
 func main() {
-	databaseName := os.Args[1]
-	db, err := sql.Open("pgx", DATABASE_URL+databaseName)
+	user := os.Args[1]
+	password := os.Args[2]
+	database := os.Args[3]
+
+	db, err := sql.Open("pgx", fmt.Sprintf(DATABASE_URL, user, password, database))
 	if err != nil {
 		logrus.Fatal("Connection failed\n", err)
 	}
 	defer db.Close()
 	scripts := reader.ReadScriptFiles()
 	// TODO execute all migrations inside one transaction.
-	createMigrationTable(db)
+	tx, err := db.Begin()
+	if err != nil {
+		logrus.Fatal("Error starting transaction.\n", err)
+	}
+
+	// Creates the basic migration table.
+	err = createMigrationTable(tx)
+	if err != nil {
+		logrus.Info("Rollbacking transacation.")
+		tx.Rollback()
+		panic(-1)
+	}
+
+	// Process all read scripts
+	err = processScripts(tx, db, scripts)
+
+	// Only commits if all operations were succesful.
+	if err != nil {
+		tx.Rollback()
+		logrus.Error("Migration executed unsuccessfully!")
+	} else {
+		tx.Commit()
+		logrus.Info("Migration executed successfully!")
+	}
+}
+
+// processScripts Process all given scripts inside a single transaction.
+func processScripts(tx *sql.Tx, db *sql.DB, scripts []reader.SQLScript) error {
 	for _, script := range scripts {
-		// TODO Check why this is happenning, ReadScriptFiles should never
-		// return and empty script.
-		if script.Name == "" {
-			logrus.Warn("Empty file\n")
-			continue
-		}
-		if !isScriptAlreadyExecuted(db, script) {
-			executeScript(db, script)
-			markScriptAsExecuted(db, script)
-		} else {
-			logrus.WithField("script_name", script.Name).Info("Script alreayd executed.\n")
+		err := processScript(tx, script)
+		if err != nil {
+			return err
 		}
 	}
-	logrus.Info("Migration executed successfully!")
+	return nil
+}
+
+// processScript Process a given script inside the given transaction.
+func processScript(tx *sql.Tx, script reader.SQLScript) error {
+	// TODO Check why this is happenning, ReadScriptFiles should never
+	// return and empty script.
+	if script.Name == "" {
+		logrus.Warn("Empty file\n")
+		return nil
+	}
+	isAlreadyProcessed, err := isScriptAlreadyExecuted(tx, script)
+	if err != nil {
+		logrus.Info("Rollbacking transacation.")
+		return err
+	}
+	if !isAlreadyProcessed {
+		err = executeScript(tx, script)
+		if err != nil {
+			logrus.Info("Rollbacking transacation.")
+			return err
+		}
+		err = markScriptAsExecuted(tx, script)
+		if err != nil {
+			logrus.Info("Rollbacking transacation.")
+			return err
+		}
+	} else {
+		logrus.WithField("script_name", script.Name).Info("Script alreayd executed.\n")
+	}
+	return nil
 }
 
 // createMigrationTable Executes the SQL script that creates the migration table.
-func createMigrationTable(db *sql.DB) {
-	tx, err := db.Begin()
-	if err != nil {
-		logrus.Fatal("Error starting transacation.\n", err)
-	}
+func createMigrationTable(tx *sql.Tx) error {
 	scriptContent, err := ioutil.ReadFile("create_migration_table.sql")
 	if err != nil {
-		logrus.Fatal("Error reading base migration file.\n", err)
+		logrus.Error("Error reading base migration file.\n", err)
+		return err
 	}
-	_, err = db.Exec(string(scriptContent))
+	_, err = tx.Exec(string(scriptContent))
 	if err != nil {
-		tx.Rollback()
-		logrus.Fatal("Error creating basic migration table.\n", err)
-	} else {
-		tx.Commit()
+		logrus.Error("Error creating basic migration table.\n", err)
+		return err
 	}
+	return nil
 }
 
 // isScriptAlreadyExecuted Check if the script was alreayd executed by counting the rows
 // in the migration table with the script name.
-func isScriptAlreadyExecuted(db *sql.DB, script reader.SQLScript) bool {
+func isScriptAlreadyExecuted(tx *sql.Tx, script reader.SQLScript) (bool, error) {
 	query := fmt.Sprintf("SELECT count(id) FROM golang_migration WHERE script_name = '%s'", script.Name)
 	var count int
-	err := db.QueryRow(query).Scan(&count)
+	err := tx.QueryRow(query).Scan(&count)
 	if err != nil {
 		logrus.WithFields(logrus.Fields{
 			"script_name": script.Name,
-		}).Fatal("Error checking if the script was alreayd executed.\n", err)
+		}).Error("Error checking if the script was alreayd executed.\n", err)
+		return false, err
 	}
-	return count > 0
+	return count > 0, nil
 }
 
 // executeScript Executes a given SQL script.
 // Every script must be executed inside a transaction.
-func executeScript(db *sql.DB, script reader.SQLScript) {
-	tx, err := db.Begin()
-	if err != nil {
-		logrus.Fatal("Error starting transaction\n", err)
-	}
+func executeScript(tx *sql.Tx, script reader.SQLScript) error {
 	logrus.Info("Executing script: ", script.Name)
-	_, err = db.Exec(script.Content)
+	_, err := tx.Exec(script.Content)
 	if err != nil {
-		tx.Rollback()
 		logrus.WithFields(logrus.Fields{
 			"script_name": script.Name,
-		}).Fatal("Error executing script.\n", err)
-	} else {
-		tx.Commit()
+		}).Error("Error executing script.\n", err)
+		return err
 	}
+	return nil
 }
 
 // markScriptAsExecuted Insert the script name on the migration table.
-func markScriptAsExecuted(db *sql.DB, script reader.SQLScript) {
-	tx, err := db.Begin()
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"script_name": script.Name,
-		}).Fatal("Error starting transaction to register script\n", err)
-	}
+func markScriptAsExecuted(tx *sql.Tx, script reader.SQLScript) error {
 	query := fmt.Sprintf("INSERT INTO golang_migration (script_name) VALUES ('%s')", script.Name)
-	_, err = db.Exec(query)
+	_, err := tx.Exec(query)
 	if err != nil {
-		tx.Rollback()
 		logrus.WithFields(logrus.Fields{
 			"script_name": script.Name,
-		}).Fatal("Error registering the executed script.\n", err)
-	} else {
-		tx.Commit()
+		}).Error("Error registering the executed script.\n", err)
+		return err
 	}
+	return nil
 }

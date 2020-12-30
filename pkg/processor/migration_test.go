07cddfe6076fae396ce50ac6efad0823cac5de39
Edison Neto <edison.aguiar.neto@gmail.com>
2020-12-30 20:00:53 -0300

    Add more test cases for fatal logs

--- pkg/processor/migration_test.go
+++ pkg/processor/migration_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/eaneto/grotto/pkg/database"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
@@ -109,6 +110,25 @@ func TestInitializeExecutorWithSuccessShouldBeginDatabaseConnection(t *testing.T
 	dbMock.ExpectBegin()
 
 	initializeExecutor(db)
+
+	assertDatabaseExpectations(t, dbMock)
+}
+
+func TestInitializeExecutorWithErrorShouldPanic(t *testing.T) {
+	db, dbMock, _ := sqlmock.New()
+	defer db.Close()
+	dbMock.ExpectBegin().WillReturnError(errors.New("Error"))
+
+	// Changes the logger for a mock that changes the value of fatal
+	// to true if the log was fatal.
+	defer func() { logrus.StandardLogger().ExitFunc = nil }()
+	var fatal bool
+	logrus.StandardLogger().ExitFunc = func(int) { fatal = true }
+
+	initializeExecutor(db)
+
+	assert.True(t, fatal)
+	assertDatabaseExpectations(t, dbMock)
 }
 
 func assertDatabaseExpectations(t *testing.T, mock sqlmock.Sqlmock) {
